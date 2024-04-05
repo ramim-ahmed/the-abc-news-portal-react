51407436477871236ccf749e22c8b3d4228be3cd
ramimcodes <ramimahmedraj2@gmail.com>
2024-04-06 02:01:10 +0600

    implement-react-hook-form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import Nav from "@/components/Nav";
-import { Link } from "react-router-dom";
+import RegisterForm from "@/components/RegisterForm";
 
 export default function Register() {
   return (
@@ -9,90 +9,7 @@ export default function Register() {
         <div>
           <section className="text-gray-600 body-font relative">
             <div className="container px-5 py-24 mx-auto flex">
-              <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:mx-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-                <h2 className="text-gray-900 text-center text-lg mb-1 font-medium title-font">
-                  Register your account
-                </h2>
-                <div className="relative mb-4">
-                  <label
-                    htmlFor="username"
-                    className="leading-7 text-sm text-gray-600"
-                  >
-                    Username
-                  </label>
-                  <input
-                    type="text"
-                    id="username"
-                    name="username"
-                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                  />
-                </div>
-                <div className="relative mb-4">
-                  <label
-                    htmlFor="email"
-                    className="leading-7 text-sm text-gray-600"
-                  >
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                  />
-                </div>
-                <div className="relative mb-4">
-                  <label
-                    htmlFor="password"
-                    className="leading-7 text-sm text-gray-600"
-                  >
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                  />
-                </div>
-                <div className="relative mb-4">
-                  <label
-                    htmlFor="confirmPassword"
-                    className="leading-7 text-sm text-gray-600"
-                  >
-                    Confirm Password
-                  </label>
-                  <input
-                    type="password"
-                    id="confirmPassword"
-                    name="confirmPassword"
-                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                  />
-                </div>
-                <div className="relative mb-4 flex items-center space-x-3">
-                  <input
-                    type="checkbox"
-                    id="confirmPassword"
-                    name="confirmPassword"
-                    className=" bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                  />
-                  <label
-                    htmlFor="confirmPassword"
-                    className="leading-7 text-sm text-gray-600"
-                  >
-                    Accept Terms And Conditions.
-                  </label>
-                </div>
-                <button className="text-white bg-primary border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                  register
-                </button>
-                <p className="text-xs text-gray-500 mt-3">
-                  Already have an account ?{" "}
-                  <Link className=" text-primary font-medium" to="/login">
-                    login
-                  </Link>
-                </p>
-              </div>
+              <RegisterForm />
             </div>
           </section>
         </div>
